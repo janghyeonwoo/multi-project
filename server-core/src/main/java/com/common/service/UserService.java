f211639209fd159135847d280ba00c420cd6a277
SMART-TN-038 <blue672@naver.com>
2023-02-28 15:55:55 +0900

    멀티 모듈 core ,api setting

--- server-core/src/main/java/com/common/service/UserService.java
+++ server-core/src/main/java/com/common/service/UserService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class UserService {
-    private final UserRepository userRepository;
+//    private final UserRepository userRepository;
 
     public String getUserName(){
         return "";
